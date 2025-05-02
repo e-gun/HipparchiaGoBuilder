a6594a546205bd9089e2661d5e2885bb7576ba2d
e-gun <egun@antisigma.classics.utoronto.ca>
2025-05-02 15:22:17 -0400

    copyright

--- support/wordcounts/scopedvariable.go
+++ support/wordcounts/scopedvariable.go
@@ -1,3 +1,8 @@
+//    HipparchiaGoBuilder
+//    Copyright: E Gunderson 2025
+//    License: GNU GENERAL PUBLIC LICENSE 3
+//        (see LICENSE in the top level directory of the distribution)
+
 package wordcounts
 
 // HeadwordLookupMap could produce a race condition; but it is read-only after write-once
